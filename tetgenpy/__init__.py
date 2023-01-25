91ae89b1e9a6527d2d6c29270ca4df09bdd7de78
Jaewook Lee <jaewooklee042@gmail.com>
2023-01-25 23:52:25 +0900

    basics

---
+++ tetgenpy/__init__.py
@@ -0,0 +1,7 @@
+from tetgenpy import _version
+from tetgenpy import tetgenpy_core as core
+from tetgenpy import plc
+from tetgenpy import tetrahedralize
+from tetgenpy.plc import PLC
+
+__version__ = _version.__version__
