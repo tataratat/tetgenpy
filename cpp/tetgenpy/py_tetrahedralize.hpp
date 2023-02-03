2de7e72af29968f83806f1c77f0ba6f7ce015ca7
Jaewook Lee <jaewooklee042@gmail.com>
2023-02-03 22:55:23 +0900

    full support for tetrahedralize call

--- cpp/tetgenpy/py_tetrahedralize.hpp
+++ cpp/tetgenpy/py_tetrahedralize.hpp
@@ -10,13 +10,27 @@ namespace tetgenpy {
 
 namespace py = pybind11;
 
-void Tetrahedralize(std::string switches, PyTetgenIo& in, PyTetgenIo& out) {
+/// Wrap tetrahedralize
+void Tetrahedralize(std::string switches,
+                    PyTetgenIo& in,
+                    PyTetgenIo& out,
+                    PyTetgenIo& additional_points,
+                    PyTetgenIo& background_mesh) {
+
+  // str to char*
   char* c_switches = &switches.front();
-  tetrahedralize(c_switches, &in, &out);
+
+  // call tetrahedralize
+  tetrahedralize(c_switches, &in, &out, &additional_points, &background_mesh);
+
+  // mark output
   in.is_output_ = false;
   out.is_output_ = true;
+  additional_points.is_output_ = false;
+  background_mesh.is_output_ = false;
 }
 
+/// reproduce main(argc, argv)
 int Main(py::list sys_argv) {
   // prepare argc
   int argc = static_cast<int>(sys_argv.size());
